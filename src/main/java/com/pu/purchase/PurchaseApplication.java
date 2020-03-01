f66c1fc6c8f182053cd0b353cff8dff6fdc14b91
ljw <ljw>
2020-03-01 15:47:23 +0800

    提交逆向工程

--- src/main/java/com/pu/purchase/PurchaseApplication.java
+++ src/main/java/com/pu/purchase/PurchaseApplication.java
@@ -1,9 +1,13 @@
 package com.pu.purchase;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+@MapperScan("com.pu.purchase")
+@EnableScheduling
 public class PurchaseApplication {
 
     public static void main(String[] args) {
